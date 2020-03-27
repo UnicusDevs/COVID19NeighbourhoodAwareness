f53c8f2bb159d14177ee41fd37377450031e1254
Mahes Varier <mahesvarier@gmail.com>
2020-03-27 12:22:34 +1100

    SignUp and Routing set-up

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+require('dotenv/config');
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+//Middleware to accept Body
+app.use(bodyParser.json() );
+
+//Region Start - Routing for Sign Up
+const signUp = require('./routes/api/signUp');
+app.use('/signUp', signUp);
+//Region End - Routing for Sign Up
+
+//Connecting to MongoDB
+mongoose.connect(process.env.DB_CONNECTION, 
+                { useNewUrlParser: true },
+                ()=>console.log("Connected to DB"))
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
