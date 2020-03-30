d18deefa529d6d423d99abef190c5ce6dea3c4fa
Max Kelly <max@revelthat.com>
2020-03-30 20:02:04 +1100

    added get user by id

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-//Item Model 
+// User Model 
 const User = require('../../models/User');
+
+// Controller
+const { getCurrentUser } = require("../../controllers/user_controller");
 
 // @route   GET api/Items
 // @desc    GET all items
@@ -11,5 +14,7 @@ router.get('/',(req,res) => {
         .sort({date: -1})
         .then(items => res.json(items))
 })
+
+router.get('/:user_id', getCurrentUser)
 
 module.exports = router;
