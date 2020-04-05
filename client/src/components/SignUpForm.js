a0e2921c57c449ececc6cb9f4c2d3b51812bf478
Max Kelly <max@revelthat.com>
2020-04-05 15:24:35 +1000

    Signup route works

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -6,7 +6,10 @@ import {useForm} from 'react-hook-form';
 // Redux
 import {connect} from 'react-redux';
 import { saveFormData } from "./../redux/actions/signupFormActions.js";
-import store from "./../redux/configureStore";
+
+// API Calls
+import {sendUserToDatabase} from './../api/sendUserToDatabase';
+
 
 let SignUpForm = props => {
 
@@ -15,35 +18,47 @@ let SignUpForm = props => {
   // The below sends the data off to the store. 
   const onSubmit = formData => {
     props.saveFormData(formData)
-  };
-
-  const onChange = (event) => {
-    // console.log(event.target.value)
+    sendUserToDatabase(formData)
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+
       <div>
-        <input name="firstName" placeholder="Josephine" type="text" onChange={onChange} ref={register} />
+        <label> First Name </label>
+        <input name="firstName" label="Hello" placeholder="Josephine" type="text" ref={register} />
       </div>
+
       <div>
-        <input onChange={onChange} name="lastName" placeholder="Smith" type="text" ref={register} />
+        <label> Last Name </label>
+        <input name="lastName" placeholder="Smith" type="text" ref={register} />
       </div>
+
       <div>
-        <input name="emailAddress" placeholder="me@example.com" type="email" ref={register} />
-      </div>
-      <div>
+        <label> Age </label>
         <input name="age" placeholder="24" type="number" ref={register} />
       </div>
+
       <div>
-        <input name="password" placeholder="******" type="password" ref={register} />
-      </div>
-      <div>
-        <input name="confirmPassword" placeholder="******" type="password" ref={register} />
-      </div>
-      <div>
+        <label> Suburb </label>
         <input name="suburb" placeholder="Richmond" type="text" ref={register} />
       </div>
+
+      <div>
+        <label> Email Address </label>
+        <input name="emailAddress" placeholder="me@example.com" type="email" ref={register} />
+      </div>
+
+      <div>
+        <label> Password </label>
+        <input name="password" placeholder="******" type="password" ref={register} />
+      </div>
+
+      <div>
+        <label> Confirm Password </label>
+        <input name="confirmPassword" placeholder="******" type="password" ref={register} />
+      </div>
+
       <button type="submit"> Submit </button>
    </form>
   );
