e60641f8719a2545bb1c483227e36a6e597b937d
Max Kelly <max@revelthat.com>
2020-04-06 15:32:12 +1000

    added validation

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -39,14 +39,16 @@ let SignUpForm = props => {
     })
   };
 
-  const {register, handleSubmit, watch, errors} = useForm();
+  const {register, handleSubmit, watch, errors} = useForm({
+    validateCriteriaMode: "all",
+    mode: "onSubmit"
+  });
 
   // The below sends the data off to the store, and calls axios function
   const onSubmit = formData => {
-    
+
     // Below saves formData to redux
     props.saveFormData(formData)
-
     // Below calls axios function
     sendUserToDatabase(formData);
   };
@@ -56,37 +58,90 @@ let SignUpForm = props => {
 
       <div>
         <label> First Name </label>
-        <input name="firstName" label="Hello" placeholder="Josephine" type="text" ref={register} />
+        <input 
+          name="firstName" 
+          label="Hello" 
+          placeholder="Josephine" 
+          type="text" 
+          ref={register({required: true, minLength: 2})} 
+        />
+
+        {errors.firstName && errors.firstName.types.required && (<p>First Name required</p>)}
+        {errors.firstName && errors.firstName.types.minLength && (<p>Name must be greater than two letters</p>)}
       </div>
 
       <div>
         <label> Last Name </label>
-        <input name="lastName" placeholder="Smith" type="text" ref={register} />
+        <input 
+          name="lastName" 
+          placeholder="Smith" 
+          type="text" 
+          ref={register({required: true, minLength: 2})} 
+        />
+
+        {errors.lastName && errors.lastName.types.required && (<p>Last Name required</p>)}
+        {errors.lastName && errors.lastName.types.minLength && (<p>last must be greater than two letters</p>)}
       </div>
 
       <div>
         <label> Age </label>
-        <input name="age" placeholder="24" type="number" ref={register} />
+        <input 
+          name="age" 
+          placeholder="24" 
+          type="number" 
+          ref={register({ required: true })} 
+        />
+
+        {errors.lastName && errors.lastName.types.required && (<p>Age required</p>)}
       </div>
 
       <div>
         <label> Suburb </label>
-        <input name="suburb" placeholder="Richmond" type="text" ref={register} />
+        <input 
+          name="suburb" 
+          placeholder="Richmond" 
+          type="text" 
+          ref={register({ required: true, minLength: 2 })} 
+        />
+
+        {errors.lastName && errors.lastName.types.required && (<p>Suburb required</p>)}
       </div>
 
       <div>
         <label> Email Address </label>
-        <input name="emailAddress" placeholder="me@example.com" type="email" ref={register} />
+        <input 
+          name="emailAddress" 
+          placeholder="me@example.com" 
+          type="email" 
+          ref={register({ required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/  })} 
+        />
+        
+        {errors.emailAddress && errors.emailAddress.types.required && (<p>Email required</p>)}
+        {errors.emailAddress && errors.emailAddress.types.pattern && (<p>Not a valid email address</p>)}
       </div>
 
       <div>
         <label> Password </label>
-        <input name="password" placeholder="******" type="password" ref={register} />
+        <input 
+          name="password" 
+          placeholder="******" 
+          type="password" 
+          ref={register({ required: true })} 
+        />
+
+        {errors.password && errors.password.types.required && (<p>Password required</p>)}
       </div>
 
       <div>
         <label> Confirm Password </label>
-        <input name="confirmPassword" placeholder="******" type="password" ref={register} />
+        <input 
+          name="confirmPassword" 
+          placeholder="******" 
+          type="password"
+          ref={register({ required: true })} 
+        />
+
+        {errors.password && errors.password.types.required && (<p>Please confirm your password</p>)}
       </div>
 
       <button type="submit"> Submit </button>
