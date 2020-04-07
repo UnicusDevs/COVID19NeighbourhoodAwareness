9e91760d66397ac21fd791019340f3446c2b0737
mahesmvp <mahesmvp94@gmail.com>
2020-04-07 17:58:02 +1000

    login form test

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import LogInForm from './../components/LogInForm';
+
 const LoginPage = () => {
   return (
     <div>
       <h1> Login Page </h1>
+      <LogInForm/>
     </div>
   )
 };
