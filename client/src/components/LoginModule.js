a2e72d71be50713163016e70b0a9a9fd5f5e2250
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-04-11 10:10:50 +1000

    Fixed file names and imports that were causing crash

--- client/src/components/LoginModule.js
+++ client/src/components/LoginModule.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import LoginForm from './LoginForm';
+import LogInForm from './LogInForm';
 
 let LoginModule = props => {
   return (
     <div>
-      <LoginForm />
+      <LogInForm />
     </div>
   )
 };
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import LoginForm from './../components/LoginForm';
+import LogInForm from '../components/LogInForm';
 
 const LoginPage = () => {
   return (
     <div>
       <h1> Login Page </h1>
-      <LoginForm/>
+      <LogInForm/>
     </div>
   )
 };
