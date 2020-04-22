334e709608501f2178048c1511edc3a8485d1b66
Max Kelly <max@revelthat.com>
2020-04-22 20:42:40 +1000

    fixes

--- client/src/api/baseURL.js
+++ client/src/api/baseURL.js
@@ -6,6 +6,6 @@ const cookies = new Cookies();
 const token = cookies.get("covid19Project");
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://home2home-covid.herokuapp.com/',
   headers: { Authorization: token }
 });
