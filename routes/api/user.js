136d62f7167382a676315df5c18aa4f3cc0d75ee
mahesvarier <mahes.varier@gmail.com>
2020-05-09 11:59:08 +1000

    Basic logic for update user.

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// User Model 
-const User = require('../../models/User');
+// User Model
+const User = require("../../models/User");
 
 // Controller
-const { getCurrentUser, getAllUsers, getUser } = require("../../controllers/user_controller");
+const {
+  getCurrentUser,
+  getAllUsers,
+  getUser,
+  editCurrentUser,
+} = require("../../controllers/user_controller");
 const { checkToken } = require("./../../controllers/authentication_controller");
 
 // @route   GET api/Items
 // @desc    GET all items
-router.get('/', getAllUsers);
-router.get('/current', checkToken, getCurrentUser);
-router.get('/:user_id', getUser);
+router.get("/", getAllUsers);
+router.get("/current", checkToken, getCurrentUser);
+router.get("/:user_id", getUser);
+router.post("/editCurrentUser", editCurrentUser);
 
 module.exports = router;
